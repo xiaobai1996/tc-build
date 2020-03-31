32deb14460502ed1d470304e1a0338d36033a38e
AtomSand <andychangzxx@outlook.com>
2020-03-31 16:57:57 +0800

    build-toolchain.sh:add all-in-one script

---
+++ build-toolchain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+print()
+{
+    echo -e "\e[1;31m$*\e[0m"	
+}
+
+print "Install dependencies"
+sudo apt install bc \
+            bison \
+            ca-certificates \
+            ccache \
+            clang \
+            cmake \
+            curl \
+            file \
+            flex \
+            gcc \
+            g++ \
+            git \
+            libelf-dev \
+            libssl-dev \
+            make \
+            ninja-build \
+            python3 \
+            texinfo \
+            u-boot-tools \
+	    lld \
+            zlib1g-dev
+
+print "Start building LLVM"
+./build-llvm.py \
+	--assertions \
+	--build-type "Release" \
+	--clang-vendor "AtomSand-$(date +%D)" \
+	--incremental \
+	--lto "thin" \
+	--targets "AArch64;ARM" \
+	--use-good-revision
+
+print "Start building binutils"
+./build-binutils.py \
+	--targets arm aarch64
